9e1058536aeb4a1475a9b68ab5e813caa4780b3a
Amos <alphatrl1997@gmail.com>
2019-09-25 23:08:53 +0800

    Redo Q4
    - Redid q4 to use lists
    - Added comments to make it easier to understand

--- Lab Test 1 (2018)/solutions/q4.py
+++ Lab Test 1 (2018)/solutions/q4.py
@@ -2,47 +2,71 @@ def print_dancing_string(sentence, start):
   # write your answer between #start and #end
   #start
   
-  string_top = '|'
-  string_mid = '|'
-  string_btm = '|'
-  string_list = [string_top, string_mid, string_btm]
+  # list_top, list_mid, list_btm is stored in a list in that order
 
+  list_top = []
+  list_mid = []
+  list_btm = []
+  string_list = [list_top, list_mid, list_btm]
+
+  # point will be used to keep track of where to save the character into the string
+  # if point = 0, save char into string_list[0]
+  # else save a space ' ' into the other strings stored in string_list
+
+  # go_up is used to determine whether the character is 'moving up' to the line above
+  # is true, point = point - 1, this means the next character in sentence should be store in string_list[point - 1]
+  # if false. point = point + 1, this means the next character should be stored in string_list[point + 1]
+
+  # save position and goUp accordingly
   if start =='T':
-    goUp = False
+    go_up = False
     point = 0
   elif start =='M':
-    goUp = True
+    go_up = True
     point = 1
   elif start == 'B':
-    goUp = True
+    go_up = True
     point = 2
 
-  for index in range(len(sentence)):
-
+  for char in sentence:
+    
+    # add character into string
     for string_index in range(len(string_list)):
+      add_char = ' '
       if string_index == point:
-        string_list[string_index] += sentence[index]
-      else:
-        string_list[string_index] += ' '
+        add_char = char
+      # now add char to the respective list
+      string_list[string_index].append(add_char)
 
-    if goUp:
-      if point == 2:
-        point -= 1
-        goUp = False
-      else:
-        point += 1
-
-    elif not goUp:
+    # update next position of char
+    # if going_up and point = 0, means i have reach the top,
+    # update going_up to false, and point to point + 1
+    if go_up:
       if point == 0:
         point += 1
-        goUp = True
+        go_up = False
       else:
-        point -= 1   
+        point -= 1
+    
+    # go_up is False
+    elif not go_up:
+      if point == 2:
+        point -= 1
+        go_up = True
+      else:
+        point += 1
+  
+  # all characters in text have been added in this point in time
+  # now we add the '|' at the front and back of each string and print 
+  for a_list in string_list:
+    a_list.insert(0, '|')
+    a_list.append('|')
 
-
-  for string in string_list:
-      string += '|'
-      print(string)
+    for char in a_list:
+      print(char, end='')
+    
+    # at end of string, break to next line
+    print()
   #end
 
 
